ca2b8644c9c492efd5d022ebe16f3bd93253b1cb
karlinexcode <etiniarchibong02@gmail.com>
2022-12-21 02:06:25 +0100

    data types

---
+++ 0x00-JavaScript_Fundamentals/data_types.js
@@ -0,0 +1,9 @@
+const isIsland = false;
+let language;
+console.log(typeof language); //undefined
+
+language = "English";
+console.log(typeof isIsland); //boolean
+console.log(typeof population); //number
+console.log(typeof country); //string
+console.log(typeof language); //string
--- 0x00-JavaScript_Fundamentals/values_and_variables.js
+++ 0x00-JavaScript_Fundamentals/values_and_variables.js
@@ -1,4 +1,11 @@
+//you can declare variables with let, var or const
+// let - allows it to be changed/updated later in the script
+//const - cannot be reassigned later in the script
+//var - this is the similar to "let" and "const" difference is that var is function scope as opposed to the others that are block scope, this means you can call var outside a block but you can't call let and const.
+
 const country = "Nigeria";
 const continent = "Africa";
-const population = "200 million";
-console.log(country, continent, population);
+let population = 200;
+console.log(country);
+console.log(continent);
+console.log(population);
