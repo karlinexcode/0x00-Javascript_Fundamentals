c77f81455c5240cdfbd428931fe3cf788c128f13
karlinexcode <etiniarchibong02@gmail.com>
2022-12-22 23:39:38 +0100

    add 4-equality_operators

---
+++ 0x00-JavaScript_Fundamentals/4-equality_operators.js
@@ -0,0 +1,23 @@
+//assignment
+//equality operators ===(strict equlity) vs ==(loose equality)
+// === operator - the prompt here has been turned to number
+const numNeighbours = Number(prompt("How many neighbour countries does your country have?"));
+if (numNeighbours === 1){
+    console.log("only 1 border");
+}else if (numNeighbours > 1){
+    console.log("more than 1 border");
+}else if (numNeighbours < 1){
+    console.log("No border");
+}
+/* the === equality does not use type conversion like the == operator */
+// == operator - the prompt here is a string
+// const numNeighbours = prompt("How many neighbour countries does your country have?");
+// if (numNeighbours == 1){
+//     console.log("only 1 border");
+// }else if (numNeighbours > 1){
+//     console.log("more than 1 border");
+// }else if (numNeighbours < 1){
+//     console.log("No border");
+// }
+// === is an equality that checkes whether the two compared values are of the same type and value
+//while == operator is a comaprison operator that only compares the values and doesn't care about the typeof
