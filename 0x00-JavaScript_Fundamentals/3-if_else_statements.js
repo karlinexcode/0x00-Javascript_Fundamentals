38807bd872bb39e928a5f686f3fad2bee3503786
karlinexcode <etiniarchibong02@gmail.com>
2022-12-22 21:34:14 +0100

    add 3-if_else_statements

---
+++ 0x00-JavaScript_Fundamentals/3-if_else_statements.js
@@ -0,0 +1,7 @@
+//Assignment 
+// if-else statement
+if (population > 33){
+    console.log(`${country}'s population is above average.`);
+}else {
+    console.log(`${country}'s population is ${33-population} million below average.`);
+}
