c06dcee028b16a9b2b3736e0d0df0f712be608bc
karlinexcode <etiniarchibong02@gmail.com>
2022-12-26 00:58:54 +0100

    7-tenary operators

---
+++ 0x00-JavaScript_Fundamentals/7-tenary_operator.js
@@ -0,0 +1,7 @@
+//assignment
+//
+/* The condition(Tenary) statment is being executed by the syntax format below:
+condition ? option if true : option if false
+*/
+
+(population > 33) ? console.log(`${country}'s population is above average`) : console.log(`${country}'s population is below average`);
