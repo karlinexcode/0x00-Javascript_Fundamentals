714b96a7eb5385f11a8ea37df0ee7d102253efe1
karlinexcode <etiniarchibong02@gmail.com>
2022-12-26 19:28:20 +0100

    add dot vs bracket notation

---
+++ 0x01-javascript_fundamentals/7-dot_vs_bracket_notation.js
@@ -0,0 +1,8 @@
+//Assignment - Dot vs Bracket Notation
+//values gotten from input in file(6-objects.js)
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}`);
+//Increase the country's population by two million using dot notation
+console.log(myCountry.population + 2);
+// decrease it by two million using brackets notation
+console.log(myCountry["population"]- 2);
