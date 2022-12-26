e821a79a70d1022fe5bb1a521afc24d817cff6a3
karlinexcode <etiniarchibong02@gmail.com>
2022-12-26 18:28:57 +0100

    add objects

---
+++ 0x01-javascript_fundamentals/6-objects.js
@@ -0,0 +1,17 @@
+//Assignment - Introduction to objects
+//when compared to Arrays in objects the order of the elements/property doesn't matter because we don't access the properties based on index as it is done in Arrays
+/* format
+
+[const] varName = {
+    objectName: objectValue,....
+};
+
+*/
+const myCountry = {
+    country: "Nigeria",
+    capital: "Abuja",
+    language: "English",
+    population: 200,
+    neighbours: ["Chile", "Guinea Bissau", "Cameroun"],
+};
+console.log(myCountry);
