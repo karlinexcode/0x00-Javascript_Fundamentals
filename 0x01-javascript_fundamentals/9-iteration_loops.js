e3d532c11f9ffd7206ce976e696488190c0a8894
karlinexcode <etiniarchibong02@gmail.com>
2022-12-27 23:49:13 +0100

    iteration-looping

---
+++ 0x01-javascript_fundamentals/9-iteration_loops.js
@@ -0,0 +1,6 @@
+// Assignment - Iteration: The for Loop
+//Loops are used to manage repitive tasks
+//voter = voter + 1 is the same thing as voter++
+for(let voter = 1; voter <= 50; voter++){
+    console.log(`Voter number ${voter} is currently voting`)
+}
